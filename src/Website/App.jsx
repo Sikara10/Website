bd9bb708c0a101a2b1bae6b5c0da3a207e145ece
unknown <arifadil739@gmail.com>
2022-10-07 01:24:28 +0500

    update

--- src/Website/App.jsx
+++ src/Website/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./Header";
-import {Routes , Route} from 'react-router-dom'
+import {Routes , Route , Navigate} from 'react-router-dom'
 import Home from "./Home";
 import Services from "./Services";
 import About from "./About";
@@ -15,6 +15,7 @@ const App = ()=>{
         <Route path="/services" element={<Services/>}  />
         <Route path="/about" element={<About/>}  />      
         <Route path="/contact" element={<Contact />}  />      
+        <Route path="/Website/" element={<Navigate to="/home" />} />
     </Routes>
     
     </>)
